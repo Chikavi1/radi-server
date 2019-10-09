6b376c6ce73b06cabf98b93061f4b47166d24cb6
chikavi1 <chikavi@hotmail.com>
2019-10-09 00:01:14 -0500

    cors problem

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,6 +19,7 @@ class Kernel extends HttpKernel
         \Radi\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \Radi\Http\Middleware\TrustProxies::class,
+        \Barryvdh\Cors\HandleCors::class,
     ];
 
     /**
@@ -41,7 +42,7 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:60,1',
             'bindings',
-            //\Barryvdh\Cors\HandleCors::class,
+            \Barryvdh\Cors\HandleCors::class,
         ],
     ];
 
